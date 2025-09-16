0260654479b8a9794ee0eb863e2268455c8c7b1c
ali-haider-dev <alihaider061297@gmail.com>
2025-09-16 00:41:23 -0700

    role base acces control

--- src/app/counter/counter.tsx
+++ src/app/counter/counter.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import React, { useState } from "react";
+import { useAuth, useUser } from "@clerk/nextjs";
+
 
 const Counter = () => {
   const [count, setCount] = useState(1);
-  if (count % 2 === 0) {
-    throw new Error("Error from Counter");
-  }
 
+  const authObj = useAuth();
+  const userObj = useUser()
+  // if (count % 2 === 0) {
+  //   throw new Error("Error from Counter");
+  // }
+console.log(authObj);
+console.log(userObj);
   return (
     <div className="flex flex-col items-center justify-center">
       <h1>This is the Counter Counter Value : {count} </h1>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Metadata } from "next";
 import Link from "next/link";
+import CounterHome from "./counter/page";
 export const metadata: Metadata = {
   title: "Home | NextJs-learning",
   description: "This is the Counter Description",
 };
 
 const Home = () => {
+
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h1>This is the default home page</h1>
@@ -14,6 +17,7 @@ const Home = () => {
       <Link href={"/counter"}>counter</Link>
       <br />
       <Link href={"/products"}>Products</Link>
+      <CounterHome/>
     </div>
   );
 };
